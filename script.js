5d960632bc78abab06c3840aa17a63603a69b3d2
Jeff <jeffozordi@gmail.com>
2026-05-02 03:42:35 +0100

    nav

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const collapsible = document.querySelectorAll(".collapsible");
+const navToggler = document.querySelector(".nav__toggler");
 collapsible.forEach((item) => 
     item.addEventListener("click", function(){
         this.classList.toggle("collapsible--expanded");
     })
 ); 
+
+navToggler.addEventListener('click', () => {
+  navToggler.classList.toggle('fa-bars')
+  navToggler.classList.toggle('fa-times')
+})
